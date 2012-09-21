df9e0f1be83236c370d9e57df8d93869a90d3d60
rail_rz <dxza@inbox.ru>
2012-09-21 21:36:24 +0400

    тест коммить

--- db.expample.php
+++ db.expample.php
@@ -2,11 +2,11 @@
 //db::init('localhost', 'root', '123', 'poll');
 $name=array(
     'a' => array(
-        'b' => 1
+        'b'  => 1
     )
 );
-echo$name["a"]["b"];
 
+echo $name["a"]["b"];
 // ответ, у которого id = 1
 //$answer = db::getInstance()->selectRow('SELECT * FROM answer WHERE id = ?', array(0));
 /**
