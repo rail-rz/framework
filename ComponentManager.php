3c75ead07d01f96696f40262ee33b04a0d2d24cd
rail_rz <zamaletdinov.rz@gmail.com>
2012-10-03 15:41:03 +0400

    compinentManager

---
+++ ComponentManager.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: R2
+ * Date: 21.09.12
+ * Time: 21:58
+ * To change this template use File | Settings | File Templates.
+ */
+class ComponentManager
+{
+
+}
