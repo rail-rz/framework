9d72dd03cca33b9f0d4101a1ee7c63173aa4e6ca
rail_rz <zamaletdinov.rz@gmail.com>
2012-10-14 22:37:09 +0400

    ComponentManager1

--- ComponentManager.php
+++ ComponentManager.php
@@ -1,12 +1,34 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
+ * ComponentManager
  * User: R2
  * Date: 21.09.12
  * Time: 21:58
  * To change this template use File | Settings | File Templates.
  */
+
 class ComponentManager
 {
+    private $components;
+    public function __construct($config)
+    {
+        $this->config=$config;
+    }
 
+    public function __get($name)
+    {
+        if (!isset($this->components[$name]))
+        {
+            $component=$this->config[$name]['class'];
+            $this->components[$name] = new $component();
+        }
+        else
+        {
+            throw new Exception('Не получилось создать объект в ComponentManager.php');
+        }
+        return $this->components[$name];
+//                $component=$this->config[$name]["class"];
+//        $component[]=new $component();
+//        return$component;
+    }
 }
