aac33e54642c97761b8904512be69de3374703e0
rail_rz <zamaletdinov.rz@gmail.com>
2012-10-19 20:12:26 +0400

    закомм. строчка require_once

--- controller/AnswerController.php
+++ controller/AnswerController.php
@@ -6,7 +6,7 @@
  * Time: 23:53
  * To change this template use File | Settings | File Templates.
  */
-require_once "models/AnswerFetcher.php";
+//require_once "models/AnswerFetcher.php";
 class AnswerController
 {
     public function  actionIndex()
