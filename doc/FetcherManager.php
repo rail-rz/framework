3e1ee395847b50d1ceb23bec30e3c3a4d571627a
rail_rz <zamaletdinov.rz@gmail.com>
2012-11-03 00:15:42 +0300

    @property

---
+++ doc/FetcherManager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * FetcherManager
+ * User: R2
+ * Date: 02.11.12
+ * Time: 22:13
+ *
+ * @property AnswerFetcher $answer
+ * @property PollFetcher $poll
+ */
+class FetcherManager extends ComponentManager
+{
+
+}
