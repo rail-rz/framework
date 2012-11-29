43cb8123aa68e7cb279f4d01eb314bbced5193fd
rail_rz <zamaletdinov.rz@gmail.com>
2012-11-29 16:16:01 +0300

    update

--- controller/Controller.php
+++ controller/Controller.php
@@ -16,13 +16,13 @@ class Controller
         $this->id=$id;
     }
 
-    public function render($view,$args=null,$return=false)
+    public function render($_view_,$_data_=null)
     {
-        if(is_array($args))
-            extract($args,EXTR_PREFIX_SAME,'data');
-        $resultRoot="views/".$this->id."/".$view.".php";
-        if(file_exists($resultRoot))
-            require ($resultRoot);
+        if(is_array($_data_))
+            extract($_data_,EXTR_PREFIX_SAME,'data');
+        $_viewFile_="views/".$this->id."/".$_view_.".php";
+        if(file_exists($_viewFile_))
+            require ($_viewFile_);
         else
             throw new Exception('no require File');
     }
