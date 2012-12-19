d26af2a5f9fdb2e0e9355c64fb17236be54b49a8
rail_rz <zamaletdinov.rz@gmail.com>
2012-12-19 16:10:59 +0300

    add header footer content

---
+++ views/layout/main.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: R2
+ * Date: 30.11.12
+ * Time: 20:48
+ * To change this template use File | Settings | File Templates.
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+</head>
+
+<div class="container">
+
+    <div id="header">
+        <div id="logo">Header</div>
+    </div><!-- header -->
+
+    <?=$content ;?>
+
+    <div id="footer">
+        <div>Footer</div>
+    </div><!-- footer -->
+
+</div><!--page>
