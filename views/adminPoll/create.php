7d26a993093c8f7ad1d1d3b0eba70d9ed3949c4d
rail_rz <zamaletdinov.rz@gmail.com>
2013-01-13 05:00:47 +0300

    add new views

---
+++ views/adminPoll/create.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: R2
+ * Date: 13.01.13
+ * Time: 5:56
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ views/adminPoll/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: R2
+ * Date: 13.01.13
+ * Time: 4:55
+ * To change this template use File | Settings | File Templates.
+ */
+?>
+<h1>Опросники</h1>
+<p><a href="/index.php?r=adminPoll/create">Создать опрос</a> </p>
+<?php
+foreach($poll as $polls)
+{
+    echo"<div>";
+    $numberPoll=$polls['id'];
+    echo$numberPoll.".".$polls['name']."<br>";
+    echo"1.<a href='/index.php?r=adminPoll/update&id=$numberPoll'>Редактировать опрос</a><br>";
+    echo"2.<a href='/index.php?r=adminAnswer/update&poll_id=$numberPoll'>Редактировать вопросы</a><br>";
+    echo"3.<a href='/index.php?r=adminPoll/delete&id=$numberPoll'>Удалить Опрос</a><br>";
+    echo"4.<a href='/index.php?r=adminPoll/check&id=$numberPoll'>Открытый/Закрытый опросник</a><br>";
+    echo"</div><br>";
+}
+?>
