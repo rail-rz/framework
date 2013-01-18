d3700a1ef67abd8848aa6e6c529910108f95e686
rail_rz <zamaletdinov.rz@gmail.com>
2013-01-18 04:05:48 +0300

    clearing trash, update query and add getLastInsertId function

--- db.php
+++ db.php
@@ -1,45 +1,34 @@
 <?php
-//db::init('localhost', 'root', '', 'poll');
 class db
 {
-//    private  static $instance;
-//    public static function getInstance()
-//    {
-//        return self::$instance;
-//    }
-//
-//    public static function init($host, $user, $password, $base)
-//    {
-//        self::$instance=new db($host,$user,$password,$base);
-//    }
 
     public function __construct($host, $user, $password, $base)
     {
-        $this->db=@mysql_connect($host,$user,$password);
+        $this->db = @mysql_connect($host,$user,$password);
         @mysql_select_db($base,$this->db);
         if(!$this->db)throw new Exception("Could not select database:".mysql_error());
     }
 
     private function __clone(){}
 
-    private function query($sql,array $params=array())
+    public function query($sql,array $params = array())
     {
         foreach($params as $var)
         {
-            $var=mysql_real_escape_string($var);
+            $var = mysql_real_escape_string($var);
             $sql = preg_replace("/\?/","'$var'", $sql,1);
         }
-        $result=@mysql_query($sql,$this->db);
+        $result = @mysql_query($sql,$this->db);
         if (!$result) throw new Exception('Неверный запрос'.mysql_error());
         return $result;
     }
 
-    public function selectRow($sql,array $params=array())
+    public function selectRow($sql,array $params = array())
     {
         $result=$this->query($sql,$params);
-        if(mysql_num_rows($result)==0)
+        if(mysql_num_rows($result) == 0)
         {
-            $result=NULL;
+            $result = NULL;
         }else{
             $result=mysql_fetch_assoc($result);
         }
@@ -48,12 +37,21 @@ class db
 
     public function selectAll($sql, array $params = array())
     {
-        $result=$this->query($sql,$params);
-        $rows=array();
-        while($row=mysql_fetch_assoc($result))
+        $result = $this->query($sql,$params);
+        $rows = array();
+        while($row = mysql_fetch_assoc($result))
         {
-            $rows[]=$row;
+            $rows[] = $row;
         }
         return$rows;
+    }
+
+    public function getLastInsertId($sql, array $params = array())
+    {
+        $result = $this->query($sql,$params);
+        if(!$result)
+            return false;
+        else
+            return mysql_insert_id();
     }
 }
