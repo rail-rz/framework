7d95d07fb564d6c2a3938147c4b3c8b8d1749ea1
rail_rz <zamaletdinov.rz@gmail.com>
2013-01-24 21:10:23 +0300

    add new function

--- controller/AdminAnswerController.php
+++ controller/AdminAnswerController.php
@@ -8,12 +8,63 @@
  */
 class AdminAnswerController extends Controller
 {
+    public function actionIndex()
+    {
+        $question = Application::getInstance()->fetchers->questions->getById( $_GET['question_id'] );
+        $answers = Application::getInstance()->fetchers->answers->getByQuestionId($_GET['question_id']);
+        if(!empty($_POST['answers']))
+        {
+            $this->saveText($question['poll_id']);
+            $questionId = $_GET['question_id'];
+            header("Location: /index.php?r=adminAnswer/index&question_id=$questionId");
+        } else {
+            $this->render( 'index', array( 'question' => $question, 'answers' => $answers));
+        }
+    }
+
     public function actionUpdate()
     {
-        $pollId=$_GET['poll_id'];
-        $questions=Application::getInstance()->fetchers->questions->getByPollId($pollId);
-        $answers=Application::getInstance()->fetchers->answers->getByPollId($pollId);
-        $this->render('update', array('questions'=>$questions,'answers'=>$answers));
+        $answer = Application::getInstance()->fetchers->answers->getById($_GET['answer_id']);
+        if(!empty($_POST['answer']))
+        {
+            Application::getInstance()->fetchers->answers->updateById( $_POST['answer'], $_GET['answer_id']);
+            $questionId = $answer['question_id'];
+            header("Location: /index.php?r=adminAnswer/index&question_id=$questionId");
+        } else {
+        $this->render( 'update', array( 'answer' => $answer));
+        }
+    }
+
+    public function actionCreate()
+    {
+        $question = Application::getInstance()->fetchers->questions->getById($_GET['question_id']);
+        if(!empty($_POST['answers']['0']) and !empty($_POST['answers']['1']))
+        {
+            $this->saveText($question['poll_id']);
+            $pollId = $question['poll_id'];
+            header("Location: /index.php?r=adminQuestion/index&poll_id=$pollId");
+        }
+        else
+        {
+            $this->render('create', array( 'question' => $question));
+        }
+    }
+
+    public function actionDelete()
+    {
+        $answer = Application::getInstance()->fetchers->answers->getById($_GET['answer_id']);
+        $questionId = $answer['question_id'];
+        Application::getInstance()->fetchers->answers->deleteById($_GET['answer_id']);
+        header("Location: /index.php?r=adminAnswer/index&question_id=$questionId");
+    }
+
+    private function saveText($question)
+    {
+        foreach($_POST['answers'] as $answers)
+        {
+            if(!empty($answers))
+                Application::getInstance()->fetchers->answers->addSting($answers, $_GET['question_id'], $question);
+        }
     }
 
 }
