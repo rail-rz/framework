a32a9ff3f7a3c5f8241402b4e2a1ea32fd0734ab
rail_rz <zamaletdinov.rz@gmail.com>
2013-08-08 20:30:46 +0300

    don`t good idea realization  poll question-answer

---
+++ views/poll/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: R2
+ * Date: 04.01.13
+ * Time: 17:33
+ * To change this template use File | Settings | File Templates.
+ */
+?>
+ <h1><?php echo $poll['name']; ?></h1>
+
+ <form method="post" action="">
+    <?php foreach($questions as $question)
+    {
+        echo "<p>".$question['question']."</p>";
+        //далее идет очень грубая работа, ее лучше уростить
+        foreach($answers as $answer)
+        {
+            if($question['id'] == $answer['question_id'])
+                echo '<input type="checkbox" name="'.$answer["id"].'" value="'.$answer['id'].'">'.$answer['answer'];
+        }
+    }?>
+     <input type="submit" value="save" />
+ </form>
+<?php var_dump($_POST);?>
