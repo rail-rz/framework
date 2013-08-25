8d37bfe946f63eeb7be940bbd8e301eca1afe812
rail_rz <zamaletdinov.rz@gmail.com>
2013-08-25 04:14:56 +0300

    update link

--- views/poll/index.php
+++ views/poll/index.php
@@ -1,3 +1,3 @@
-<p><a href='/index.php?r=adminPoll/index'> Admin</a></p>
-<p>Poll</p>
-<p>Vote</p>
+<p><a href='/index.php?r=adminPoll/index'> Admin </a></p>
+<p><a href='/index.php?r=poll/sendPoll'> Poll </a></p>
+<p><a href='/index.php?r=vote/index'> Vote </a></p>
